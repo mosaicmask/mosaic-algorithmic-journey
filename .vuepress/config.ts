28a408b5f522b40b859b53cb7cc0bcbfce4efee7
赵兴 <mosaic.bilibili@gmail.com>
2023-05-23 16:45:26 +0800

    first commit

---
+++ .vuepress/config.ts
@@ -0,0 +1,45 @@
+import { defineUserConfig } from "vuepress";
+import type { DefaultThemeOptions } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+
+export default defineUserConfig({
+  title: "Algorithmic journey",
+  description: "这是捡破烂的小z的算法学习之旅，希望能给在算法学习路上迷茫的你，带来一点点参考。",
+  theme: recoTheme({
+    // home: '/Home',
+    logo: "/logo.png",
+    style: "@vuepress-reco/style-default",
+    author: "捡破烂的小z",
+    authorAvatar: "/head.png",
+    docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+    docsBranch: "main",
+    docsDir: "example",
+    lastUpdatedText: "",
+    // series 为原 sidebar
+    series: {
+      "/docs/data-structure/": [
+        {
+          text: "前言",
+          children: ["home", "theme"],
+        },
+        {
+          text: "module two",
+          children: ["api", "plugin"],
+        },
+      ],
+    },
+    navbar: [
+      // { text: "首页", link: "/Home" },
+      // { text: "Categories", link: "/categories/reco/1/" },
+      {
+        text: "旅程",
+        children: [
+          { text: "基本数据结构", link: "/docs/data-structure/home" },
+          { text: "算法解析", link: "/blogs/other/guide" },
+        ],
+      },
+      { text: "算法练习", link: "/tags/tag1/1/" },
+    ],
+  }),
+  // debug: true,
+});
---
+++ README.md
@@ -0,0 +1,22 @@
+---
+home: true
+modules:
+  - BannerBrand
+  - MdContent
+  - Footer
+bannerBrand:
+  bgImage: '/bg.svg'
+  title: Algorithmic journey
+  description: 算法旅程
+  tagline: 这是捡破烂的小z的算法学习之旅，希望能给在算法学习路上迷茫的你，带来一点点参考。
+  buttons:
+    - { text: 快速开始, link: '/docs/data-structure/home' }
+  socialLinks:
+    - { icon: 'LogoGithub', link: 'https://github.com/vuepress-reco/vuepress-theme-reco' }
+blog:
+  socialLinks:
+    - { icon: 'LogoGithub', link: 'https://github.com/recoluan' }
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+---
